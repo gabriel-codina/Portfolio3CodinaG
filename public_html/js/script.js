2a90ce191221f31da1316a1419964cd688159a33
Gabriel Codina <gabeeecodina@gmail.com>
2015-01-08 15:00:52 -0800

    added bootstrap

--- public_html/js/script.js
+++ public_html/js/script.js
@@ -1,4 +1,6 @@
 $("document").ready(function(){
-    $(".yeah").css("background-color", "yellow");
+    $(".yeah").css({"background-color":"black","border":"2px solid red"});
+    $(".bodi").css("background-image", "url('http://planezen.com/images/wall1/cool-background-hd-widescreen-11.jpg')");
+    $(".link").css({"background-color":"blue","padding":"0px","margin":"0px","width":"200px","border":"2px solid black"});
     
 });
