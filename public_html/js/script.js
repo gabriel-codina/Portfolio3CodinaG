f6662a87b68dec61e26af21fbf3e43c9be3b5b4a
Gabriel Codina <gabeeecodina@gmail.com>
2015-01-09 15:01:41 -0800

    added all pages

--- public_html/js/script.js
+++ public_html/js/script.js
@@ -1,6 +1,23 @@
 $("document").ready(function(){
     $(".yeah").css({"background-color":"black","border":"2px solid red"});
-    $(".bodi").css("background-image", "url('http://planezen.com/images/wall1/cool-background-hd-widescreen-11.jpg')");
-    $(".link").css({"background-color":"blue","padding":"0px","margin":"0px","width":"200px","border":"2px solid black"});
+    $(".bodi").css("background-image", "url('http://willpaper.com/download/32448-wallpaper-3d-hd-cars-1366x768.jpg')");
+    $(".link1",".link2",".link3").css({"background-color":"blue","padding":"0px","margin":"0px","width":"200px","border":"2px solid black"});
+    $("p").css("color","white");
+    $("h1").css("color","white");
+    $(".link1",".link2",".link3").bind('mouseover', mouseOverMe).bind('mouseout', mouseOutMe);
+    
+    $(".link1",".link2",".link3").bind('click', mouseClick);
+    
+    function mouseOverMe(){
+        $(".link1",".link2",".link3").css("background-color","red");
+    }
+    
+    function mouseOutMe(){
+        $(".link1",".link2",".link3").css("background-color","blue");
+    }
+    
+    function mouseClick(){
+        
+    }
     
 });
